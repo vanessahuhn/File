79f6c4cf6ab6c7437e4e450e51ffc8221ff5f4c8
vanessahuhn <vanessa.huhn@gmail.com>
2018-10-15 14:45:32 +0200

    path to temp directory

--- src/Exo.java
+++ src/Exo.java
@@ -4,6 +4,8 @@ public class Exo {
 	public static void main(String[] args) {
 		// permet d'afficher le chemin absolu vers le répertoire courant
 		System.out.println(System.getProperty("user.dir"));
+		//permet d'afficher les chemin vers le dossier des fichiers temporaires de java
+		System.out.println(System.getProperty("java.io.tmpdir"));
 	}
 
 }

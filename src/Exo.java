528ef556251c2f79811e65ab88bb35477c7345d2
vanessahuhn <vanessa.huhn@gmail.com>
2018-10-15 14:06:22 +0200

    print file path

---
+++ src/Exo.java
@@ -0,0 +1,9 @@
+
+public class Exo {
+
+	public static void main(String[] args) {
+		// permet d'afficher le chemin absolu vers le répertoire courant
+		System.out.println(System.getProperty("user.dir"));
+	}
+
+}

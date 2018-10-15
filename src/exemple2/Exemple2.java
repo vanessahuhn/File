411ac08b73552df8a37366b0c19b38863bd41401
vanessahuhn <vanessa.huhn@gmail.com>
2018-10-15 15:34:55 +0200

    lister les lecteurs

---
+++ src/exemple2/Exemple2.java
@@ -0,0 +1,14 @@
+package exemple2;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Exemple2 {
+	public static void main(String[] args) {
+		//liste des éléments racine du système de fichier courant
+		File [] roots = File.listRoots();
+		// affichage du tableau résultat
+		System.out.println(Arrays.toString(roots));
+	}
+
+}
